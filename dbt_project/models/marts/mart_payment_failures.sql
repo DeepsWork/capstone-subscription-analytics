b7755a69850d1fc9ba56b92e563e710825530f7d
DeepsWork <deep.shah@krishtechnolabs.com>
2026-04-02 12:31:07 +0530

    Refactor SQL query for payment failures recovery

--- dbt_project/models/marts/mart_payment_failures.sql
+++ dbt_project/models/marts/mart_payment_failures.sql
@@ -1,3 +1,41 @@
+-- WITH failures AS (
+--     SELECT
+--         payment_id,
+--         subscription_id,
+--         payment_ts   AS failed_at,
+--         amount,
+--         failure_reason
+--     FROM {{ ref('stg_payments') }}
+--     WHERE payment_status = 'failed'
+-- ),
+-- recoveries AS (
+--     SELECT
+--         p.subscription_id,
+--         MIN(p.payment_ts) AS recovered_at
+--     FROM {{ ref('stg_payments') }} p
+--     INNER JOIN failures f
+--         ON p.subscription_id = f.subscription_id
+--         AND p.payment_status = 'success'
+--         AND p.payment_ts > f.failed_at
+--         AND p.payment_ts <= f.failed_at + INTERVAL '7 days'
+--     GROUP BY 1
+-- )
+-- SELECT
+--     f.payment_id,
+--     f.subscription_id,
+--     f.failed_at,
+--     f.amount,
+--     f.failure_reason,
+--     r.recovered_at,
+--     CASE
+--         WHEN r.recovered_at IS NOT NULL THEN true
+--         ELSE false
+--     END AS recovered_within_7_days,
+--     DATEDIFF('day', f.failed_at, r.recovered_at) AS days_to_recovery
+-- FROM failures f
+-- LEFT JOIN recoveries r ON f.subscription_id = r.subscription_id
+
+
 WITH failures AS (
     SELECT
         payment_id,
@@ -31,6 +69,6 @@ SELECT
         WHEN r.recovered_at IS NOT NULL THEN true
         ELSE false
     END AS recovered_within_7_days,
-    DATEDIFF('day', f.failed_at, r.recovered_at) AS days_to_recovery
+    DATEDIFF(r.recovered_at, f.failed_at) AS days_to_recovery
 FROM failures f
 LEFT JOIN recoveries r ON f.subscription_id = r.subscription_id
